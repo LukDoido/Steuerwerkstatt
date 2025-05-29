9642dd283412a03d3e450037a5c66b7fa67833c7
LukDoido <lucas.banholzer@outlook.com>
2025-05-29 15:11:52 +0200

    Update logo URL

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -9,7 +9,7 @@ const Logo: React.FC<LogoProps> = ({ className = '' }) => {
   return (
     <Link to="/" className={`block ${className}`}>
       <img 
-        src="https://cdn.discordapp.com/attachments/353314778141294596/1375784287663820861/logo_Steuerwerkstatt.png?ex=683443f7&is=6832f277&hm=814db90e63c5579ca4fff3544cbea702656571ff20e3f2086e3bbbb1804e401e" 
+        src="https://i.imgur.com/ZwuXnNJ.png" 
         alt="Steuerwerkstatt Logo" 
         className="h-12 w-auto"
       />
